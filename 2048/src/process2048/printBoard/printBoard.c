01f42e2e704eb0a42ebff3b356a96608a20d8e0d
aiutarsi <endofthe20thcenturyhk@gmail.com>
2022-02-05 11:07:02 +0900

    making printBoard

---
+++ 2048/src/process2048/printBoard/printBoard.c
@@ -0,0 +1,7 @@
+#include "printBoard.h"
+#include <stdio.h>
+
+void printBoard(int boardSize, int board[][8], int score, int highScore, int judgeDirections[]) {
+  int colorList[20] = {136,208,196,200,126,93,63,21,33,45,76,28,64,69,97,88,131,240,232}; /* TODO : for 2^31-1, need more color and beautiful to see */
+  
+}
---
+++ 2048/src/process2048/printBoard/printBoard.h
@@ -0,0 +1,6 @@
+#ifndef _PRINT_BOARD_H_
+#define _PRINT_BOARD_H_
+
+void printBoard(int, int[][8], int, int, int*);
+
+#endif
